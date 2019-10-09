79810b03d09da29e1c81d814a956a729ee04fffa
Rizki Aria Putra <55823091+vendetta78@users.noreply.github.com>
2019-10-10 01:03:35 +0700

    Add files via upload

---
+++ PBO1-10118031-Latihan18-Kelipatan/build/classes/pbo1/pkg10118031/latihan18/kelipatan/PBO110118031Latihan18Kelipatan.rs
@@ -0,0 +1,1 @@
+kelipatan.Kelipatan
---
+++ PBO1-10118031-Latihan18-Kelipatan/src/Kelipatan.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author Vendetta 
+ * NAMA              : Rizki Aria Putra 
+ * KELAS             : PBO1 
+ * NIM               : 10118031
+ * Deskripsi Program : Program ini berisi program untuk Kelipatan
+ */
+public class Kelipatan {
+
+    public static void main(String[] args) {
+
+        double a = 3.5;
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.println(+a * i);
+            i++;
+        }
+    }
+
+}
